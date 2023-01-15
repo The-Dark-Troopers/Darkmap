85efbe095c3522e69cd5ee298d08e7f56f13b2fc
TheDarkTroopers <100459961+TheDarkTroopers@users.noreply.github.com>
2023-01-15 00:50:00 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,5 +5,8 @@ lblue="\e94m"
 echo -e "${blue}Instalando cosillas jijiji...."
 
 pip install python-nmap
+pip install colorama
+pip install time
+pip install pycryptodome
 
 echo -e "${blue}Se instalaron con exito :D"
